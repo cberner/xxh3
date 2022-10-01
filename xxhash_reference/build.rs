7d664265bd68ce191da39c19edd46de3b2d6352f
Jiseok CHOI <jiseok.dev@gmail.com>
2022-10-01 13:41:58 +0900

    Support `aarch64` arch in `xxhash_reference` build script

--- xxhash_reference/build.rs
+++ xxhash_reference/build.rs
@@ -1,6 +1,17 @@
+use cfg_if::cfg_if;
+
 fn main() {
-    cc::Build::new()
-        .file("xxHash/xxhash.c")
-        .flag("-march=native")
-        .compile("xxhash");
+    cfg_if! {
+        if #[cfg(all(target_os = "macos", target_arch = "aarch64"))] {
+            cc::Build::new()
+                .file("xxHash/xxhash.c")
+                .flag("-mcpu=apple-m1")
+                .compile("xxhash");
+        } else {
+            cc::Build::new()
+                .file("xxHash/xxhash.c")
+                .flag("-march=native")
+                .compile("xxhash");
+        }
+    }
 }
