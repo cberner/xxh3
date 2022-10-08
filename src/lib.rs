c011be35c0f3603fb75c0ee8ac3e2b131163165c
Jiseok CHOI <jiseok.dev@gmail.com>
2022-10-08 12:14:44 +0900

    Enable the `cast_*` clippy option to deny (#7)
    * Update dependencies
    
    * Enable the `cast_*` clippy option to deny
    
    * Support `aarch64` arch in `xxhash_reference` build script
    
    * Apply review comments
    
    Co-authored-by: Christopher Berner <christopherberner@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+#![deny(clippy::cast_possible_truncation)]
+#![deny(clippy::cast_possible_wrap)]
+#![deny(clippy::cast_precision_loss)]
+#![deny(clippy::cast_sign_loss)]
+
 mod xxh3;
 
 pub use crate::xxh3::{hash128_with_seed, hash64_with_seed};
