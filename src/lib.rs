2e058d00cddd52d77222d329742bafacaf73f2df
Christopher Berner <me@cberner.com>
2025-06-17 14:03:45 -0700

    Split python bindings into subcrate

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,5 @@
 #![deny(clippy::cast_precision_loss)]
 #![deny(clippy::cast_sign_loss)]
 
-#[cfg(feature = "python")]
-pub use crate::python::xxh3;
-
-#[cfg(feature = "python")]
-mod python;
-#[cfg(not(feature = "python"))]
 mod xxh3;
-
-#[cfg(not(feature = "python"))]
 pub use crate::xxh3::{hash64_with_seed, hash128_with_seed};
