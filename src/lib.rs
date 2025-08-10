40b86ff9d3ba89183825233bc0593be8039fdca4
Christopher Berner <me@cberner.com>
2025-08-10 14:01:12 -0700

    Enable pedantic clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![deny(clippy::all, clippy::pedantic, clippy::disallowed_methods)]
 #![deny(clippy::cast_possible_truncation)]
 #![deny(clippy::cast_possible_wrap)]
 #![deny(clippy::cast_precision_loss)]
@@ -5,6 +6,11 @@
 #![deny(clippy::cast_lossless)]
 #![deny(clippy::semicolon_if_nothing_returned)]
 #![deny(clippy::ptr_as_ptr)]
+// We use intrinsics without alignment requirements, so these casts are all just fine
+#![allow(clippy::cast_ptr_alignment)]
+#![allow(clippy::wildcard_imports)]
+#![allow(clippy::unreadable_literal)]
+#![allow(clippy::similar_names)]
 
 mod xxh3;
 pub use crate::xxh3::{hash64_with_seed, hash128_with_seed};
