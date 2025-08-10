b333da137048910a31ba985ca95b05cb0c37613f
Christopher Berner <me@cberner.com>
2025-08-10 13:52:25 -0700

    Enable deny(clippy::cast_lossless)

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![deny(clippy::cast_possible_wrap)]
 #![deny(clippy::cast_precision_loss)]
 #![deny(clippy::cast_sign_loss)]
+#![deny(clippy::cast_lossless)]
 
 mod xxh3;
 pub use crate::xxh3::{hash64_with_seed, hash128_with_seed};
