1b46e9dbf7ebe2cba79fa4357d678353cbc4fdf3
Christopher Berner <me@cberner.com>
2025-08-10 13:55:36 -0700

    Enable deny(clippy::semicolon_if_nothing_returned)

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![deny(clippy::cast_precision_loss)]
 #![deny(clippy::cast_sign_loss)]
 #![deny(clippy::cast_lossless)]
+#![deny(clippy::semicolon_if_nothing_returned)]
 
 mod xxh3;
 pub use crate::xxh3::{hash64_with_seed, hash128_with_seed};
